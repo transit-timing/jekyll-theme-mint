e58bd519295e381bdd46b47bba316e4cf274d761
transit-timing <97137767+transit-timing@users.noreply.github.com>
2022-01-18 09:22:35 -0500

    Update planets.js

--- js/planets.js
+++ js/planets.js
@@ -4,6 +4,9 @@ let planets = {
     "Marssss": "https://mars.nasa.gov/",
     "Jupiter": "jupiter.html",
     "WASP-12": "wasp-12.html"
+    "CoRoT-01": "CoRoT-01.html"
+    "CoRoT-02": "CoRoT-02.html"
+    "CoRoT-05": "CoRoT-05.html"
 };
 
 let planetData = {
